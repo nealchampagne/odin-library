7cd923821c021e7f7d8e5c3ed8c397493b063d75
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-06-20 16:19:44 -0700

    Initialize files for Library project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Odin Library
+
+## Description
+This is my implementation of the Odin Library project from the Javascript Course as part of the Odin Project's Full Stack Javascript curriculum.
---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+};
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+};
